0ae6da17d9e9658d3599d643364f311ba96561bf
[Miracle] Anh Nguyen Hoang <anh.nguyenhoang01@axonactive.com>
2024-06-07 21:49:56 +0700

    fix issue can not build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import storiesRoute from './routes/stories';
+import path from 'node:path';
 const app = express();
 const port = 3000;
 
@@ -13,7 +14,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use(express.static('public'));
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+});
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
